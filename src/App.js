3e23e0ead0f9731612b6786fb3502df28340b67a
McThuggets <dylan.reuben1509@gmail.com>
2023-06-08 13:19:27 +0200

    redirect when logged out

--- src/App.js
+++ src/App.js
@@ -29,18 +29,16 @@ function App() {
        <BrowserRouter>
 
            <div className="nav">
-              { isAuth ? <button className="auth-button" onClick={ () => setIsAuth(!isAuth)}> Log out </button> : <Link to="/login" className="auth-button" onClick={ () => setIsAuth(!isAuth)}> Log In </Link> }
+              { isAuth ? <Link to="/login" className="auth-button" onClick={ () => setIsAuth(!isAuth)}> Log out </Link> : <Link to="/login" className="auth-button" onClick={ () => setIsAuth(!isAuth)}> Log In </Link> }
               <Link to="/Fruits" className="auth-button"> Fruits </Link>
               <Link to="/Survey" className="auth-button"> Survey </Link>
               <Link to="/DownloadSurveys" className="auth-button"> Download Surveys </Link>
             </div>
 
            <Routes>
-
-
-                 <Route exact path='/login' element={<Login authHandler={ onLogIn }/>}></Route>
-                 <Route exact path='/Fruits' element={<Fruits />}></Route>
-                 <Route exact path='/Survey' element={<Survey />}></Route>
+                 <Route exact path='/login' element={<Login authHandler={ onLogIn } /> } > </Route>
+                 <Route exact path='/Fruits' element={<Fruits />}> </Route>
+                 <Route exact path='/Survey' element={<Survey />}> </Route>
                  <Route exact path='/DownloadSurveys' element={<DownloadSurveys />}></Route>
           </Routes>
        </BrowserRouter>
