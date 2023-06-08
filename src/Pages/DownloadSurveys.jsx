bad09ce8a443c88a08c1dd44bb281a6ce301312e
McThuggets <dylan.reuben1509@gmail.com>
2023-06-08 10:53:07 +0200

    CSV download

--- src/Pages/DownloadSurveys.jsx
+++ src/Pages/DownloadSurveys.jsx
@@ -1,12 +1,15 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import DisplaySurvey from '../Components/DownloadSurveys/DisplaySurvey'
+import { AiOutlineDownload } from "react-icons/ai";
+import { CSVLink, CSVDownload } from 'react-csv'
 
 export default function DownloadSurveys() {
 
   const [surveyData, setSurveyData] = useState([])
   const [surveyUiList, setSurveyUiList] = useState()
 
+  //get survey responses from db
   useEffect( () => {
     axios.get('https://localhost:7200/api/Surveys')
       .then(response => {
@@ -17,6 +20,8 @@ export default function DownloadSurveys() {
       })
   }, [])
   
+
+  //set ui
   useEffect(() => {
 
     setSurveyUiList(
@@ -24,10 +29,27 @@ export default function DownloadSurveys() {
     )
 
   }, [surveyData])
-  
+
+  const handleDownload = () => {
+
+
+    console.log('hi from download');
+  };
 
   return (
     <>
+        <div className='download-card'>
+
+        <div className='column card-button-group-2'>
+            <h3> Download survey responses </h3>
+        </div>
+
+        <div className='column card-button-group'>
+            <CSVLink className='card-button download' data={surveyData} filename={'surveys.csv'}>  <AiOutlineDownload /> </CSVLink>
+        </div>  
+
+        </div>
+
         {surveyUiList}
     </>
   )
